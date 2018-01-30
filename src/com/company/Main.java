beff48d52de5b564d6f7007be82a8ed1e0fa0590
Karlas <karolis.zagarnauskas@gmail.com>
2018-01-30 15:16:27 +0200

    first commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,27 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        System.out.println("Sveiki, iveskite du sveikuosius skaicius");
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Iveskite pirmaji skaiciu");
+
+        int a = sc.nextInt();
+
+        System.out.println("Iveskite antraji skaiciu");
+
+        int b = sc.nextInt();
+
+        System.out.println("Ju suma");
+
+        System.out.println(a + b);
+
+
     }
 }
